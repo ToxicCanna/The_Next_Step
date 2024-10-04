3eab43aa7af96d9888a5f87ebc96b4f02e313f5c
ToxicCanna <freedom22140@gmail.com>
2024-10-03 20:18:28 -0600

    Build is Working

---
+++ Assets/Scripts/Rampage/Enemies/HeliLogic.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HeliLogic : MonoBehaviour
+{
+    [SerializeField] private EnemyObject EnemyObject;
+    private float bulletTimer;
+    private static int enemyCount = 0;
+    [SerializeField] private Transform bulletSpawn;
+    [SerializeField] Animator anim;
+
+    private AudioManager audioManager;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        bulletTimer = Random.Range(EnemyObject.MinShootInterval, EnemyObject.MaxShootInterval);
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
+    void Update()
+    {
+        Move();
+        Shoot();
+    }
+    public void GetDamage()
+    {
+        //EnemyObject.EnemyCount--;
+        GameObject explosion = Instantiate(EnemyObject.ExplosionPrefab, bulletSpawn.position, Quaternion.identity);
+        Destroy(explosion, 1f);
+        DestroyEnemy();
+        audioManager.PlaySFX(audioManager.Explosion);
+    }
+    public void DestroyEnemy()
+    {
+        Destroy(gameObject);
+
+        //load next scene when all the enemies die
+        enemyCount++;
+        if (enemyCount >= 18)
+        {
+            SceneManager.LoadScene("BossScene");
+            audioManager.PlaySFX(audioManager.LevelChange);
+        }
+    }
+    public void Move()
+    {
+        //constant movement
+        transform.Translate(Vector2.right * EnemyObject.MinMoveSpeed * Time.deltaTime);
+    }
+
+    public void Respawn()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Shoot()
+    {
+        bulletTimer -= Time.deltaTime;
+        if (bulletTimer <= 0)
+        {
+            Instantiate(EnemyObject.BulletPrefab, transform.position, Quaternion.identity);
+            bulletTimer = Random.Range(EnemyObject.MinShootInterval, EnemyObject.MaxShootInterval);
+            anim.SetTrigger("IsAttacking");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Boundary")
+        {
+            //change the direction when boundary touched
+            EnemyObject.MinMoveSpeed *= -1;
+        }
+    }
+}
